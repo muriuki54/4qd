2a4aeb486aa1dd3b410480d06d4e34031d23dbf4
unknown <muriukicharles54@gmail.com>
2021-09-28 18:11:57 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+const tabSelectors = document.querySelectorAll(".tab_selector");
+const controllerTabs = document.querySelectorAll(".controller_tab");
+
+const innerTabs = document.querySelectorAll(".tab_inner_details");
+
+/** TABS */
+Object.keys(tabSelectors).forEach(key => {
+    tabSelectors[key].addEventListener("click", () => {
+        // reset all tabs to display none
+        Object.keys(controllerTabs).forEach(key => {
+            controllerTabs[key].classList.remove("show");
+        });
+
+        Object.keys(tabSelectors).forEach(key => {
+            tabSelectors[key].classList.remove("current");
+        });
+
+        hightLightCurrentTab(+key);
+        showTab(+key);
+    })
+})
+
+function showTab(key) {
+    controllerTabs[key].classList.add("show");
+}
+
+function hightLightCurrentTab(key) {
+    tabSelectors[key].classList.add("current");
+}
+
+// Highlight tab1 initially
+hightLightCurrentTab(0);
+
+// show the 1st tab initially
+showTab(0);
+
+/** END TABS */
+
+/** INNER TABS */
+
+Object.keys(innerTabs).forEach(key => new Slide(+key));
+console.log(innerTabs);
+function Slide(key) {
+    this.size = controllerTabs[key].clientWidth;
+    this.startIndex = 0;
+    this.stepsIndicator = controllerTabs[key].querySelector(".controller_tab_steps_indicator");
+    this.innerTabs = controllerTabs[key].querySelectorAll(".inner_tab");
+    this.steps = controllerTabs[key].querySelectorAll(".step");
+    this.goToPreviousButton = controllerTabs[key].querySelector(".inner_tab_controls .go_to_previous");
+    this.goToNextButton = controllerTabs[key].querySelector(".inner_tab_controls .go_to_next");
+
+    this.translateX= function() {
+        console.log(key);
+        innerTabs[key].style.transform = "translate("+this.startIndex * -this.size+"px)";
+        this.stepsIndicator.style.backgroundSize = ""+(this.startIndex/(this.innerTabs.length-1))*100+"% 100%";
+        
+        Object.keys(this.steps).forEach(key => this.steps[+key].classList.remove("current"));
+
+        for(let i = 0; i < this.startIndex + 1; i++) {
+            this.steps[i].classList.add("current");
+        }
+    }
+    
+    this.goToNextButton.addEventListener("click", () => {
+        if(this.startIndex >= this.innerTabs.length - 1) return;
+        this.startIndex += 1;
+        this.translateX();
+    })
+
+    this.goToPreviousButton.addEventListener("click", () => {
+        if(this.startIndex <= 0) return;
+        this.startIndex -= 1;
+        this.translateX();
+    })
+}
+
+/** END INNER TABS */
